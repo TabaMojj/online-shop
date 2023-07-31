1a352a348087eaa136e97c89c52db548d7b98357
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-07-31 19:37:14 +0300

    feat: add admin order detail view

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from .forms import OrderCreateForm
 from cart.cart import Cart
-from .models import OrderItem
+from .models import OrderItem, Order
 from orders.tasks import order_created
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from django.urls import reverse
 
 
 def order_create(request):
@@ -16,7 +18,14 @@ def order_create(request):
                                          price=item['price'], quantity=item['quantity'])
             cart.clear()
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html', {'order': order})
+            request.session['order_id'] = order.id
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
         return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
