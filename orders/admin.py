8867cdb3f1f721353ed8e8e4414403b4271355b4
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-07-31 19:36:17 +0300

    feat: add order detail to admin page

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,12 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import reverse
+
 from .models import OrderItem, Order
+from django.utils.safestring import mark_safe
 
 
 class OrderItemInLine(admin.TabularInline):
@@ -7,10 +14,47 @@ class OrderItemInLine(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+
+order_payment.short_description = 'Stripe payment'
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
+
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    order_payment, 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInLine]
+    actions = [export_to_csv]
