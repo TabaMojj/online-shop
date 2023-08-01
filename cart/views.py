bc64280226715b33af51ef777f377b2ccfa54d2c
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-01 11:20:28 +0200

    feat: add coupon form to cart detail view

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
+from coupons.forms import CouponApplyForm
 from shop.models import Product
 from .forms import CartAddProductForm
 from .cart import Cart
@@ -28,4 +29,5 @@ def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
