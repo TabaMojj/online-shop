c98080871804023e128779eba74e8497327f371f
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-01 14:50:19 +0330

    feat: add recommender system

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 from coupons.forms import CouponApplyForm
 from shop.models import Product
+from shop.recommender import Recommender
 from .forms import CartAddProductForm
 from .cart import Cart
 
@@ -30,4 +31,11 @@ def cart_detail(request):
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override': True})
     coupon_apply_form = CouponApplyForm()
-    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    if cart_products:
+        recommended_products = r.suggest_products_for(cart_products, max_result=4)
+    else:
+        recommended_products = []
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form, 'recommended_products': recommended_products})
