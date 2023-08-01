e49174814216c5e2e2e8005c5d70da0e5c577359
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-01 11:23:24 +0200

    feat: add coupon to Order model

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,8 @@
+from _decimal import Decimal
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from coupons.models import Coupon
 from shop.models import Product
 
 
@@ -14,6 +17,8 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
+    coupon = models.ForeignKey(Coupon, related_name='orders', null=True, blank=True, on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ['-created']
@@ -24,14 +29,23 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
 
-    def get_total_cost(self):
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
+
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
 
     def get_stripe_url(self):
         if not self.stripe_id:
             return ''
         if '_test_' in settings.STRIPE_SECRET_KEY:
-            # Stripe path for test payments
             path = '/test/'
         else:
             path = '/'
